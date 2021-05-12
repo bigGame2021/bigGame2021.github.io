7fbece4b81a0b6b123f24a4f76acc03f27e5b874
valeriewirianto <valerie.wirianto@mail.utoronto.ca>
2021-05-11 21:59:18 -0400

    semi final

---
+++ page_4.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    alert('DOM ready!');
+    //console.log("here");
+});
+
+function door_lock(){
+    let input = document.getElementById("b_door_input1").value;
+    if(input.toLowerCase() === "jesus loves you" | input.toLowerCase() === "jesuslovesyou"){
+        alert("The door opened. Freedom here we come!");
+        window.open("email1wooooooo.html");
+    }
+    else{
+        alert("The door isn't budging...");
+    }
+}

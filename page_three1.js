64a19a87ca3dcf6ce9cd71b75ec0e048f5f7ef48
valeriewirianto <valerie.wirianto@mail.utoronto.ca>
2021-05-11 20:36:32 -0400

    functionality of b door lv3

--- page_three1.js
+++ page_three1.js
@@ -1,3 +1,9 @@
+
+document.addEventListener('DOMContentLoaded',()=>{
+        alert('DOM ready!');
+        //console.log("here");
+    });
+
 function a_chest_lock(){
     let input1 = document.getElementById("a_chest_input1");
     let input2 = document.getElementById("a_chest_input2");
@@ -73,19 +79,22 @@ function b_door_lock(){
     let input1 = document.getElementById("b_door_input1");
     let input2 = document.getElementById("b_door_input2");
     let input3 = document.getElementById("b_door_input3");
+    let input4 = document.getElementById("b_door_input4");
     let answer1;
     let answer2;
     let answer3;
+    let answer4;
     try {
         answer1 = parseInt(input1.value);
         answer2 = parseInt(input2.value);
         answer3 = parseInt(input3.value);
+        answer4 = parseInt(input4.value);
         
     } catch (error) {
         alert("The door isn't budging... perhaps I should try a different answer?");
     }
 
-    if(answer1 === 5 && answer2 === 2 && answer3 === 9){
+    if(answer1 === 1 && answer2 === 1 && answer3 === 1 && answer4 === 1){
         alert("The door opened!");
         window.open("dialogue2bqwerty.html");
     }

e5272418b9c53fcfe54e5cd07590f786f6108458
valeriewirianto <valerie.wirianto@mail.utoronto.ca>
2021-05-11 23:14:30 -0400

    finale

--- page_4.js
+++ page_4.js
@@ -3,7 +3,7 @@ function door_lock(){
     let input = document.getElementById("b_door_input1").value;
     if(input.toLowerCase() === "jesus loves you" | input.toLowerCase() === "jesuslovesyou"){
         alert("The door opened. Freedom here we come!");
-        window.open("email1wooooooo.html");
+        window.open("FINALE_YAY.html");
     }
     else{
         alert("The door isn't budging...");
